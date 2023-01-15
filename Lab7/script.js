86c9cb1f48966b94cda2b1276c163168143ce569
MvxJ <58326571+MvxJ@users.noreply.github.com>
2023-01-15 20:32:45 +0100

    Correct auto sugest, Style main weather data, Inject charts

--- Lab7/script.js
+++ Lab7/script.js
@@ -27,23 +27,59 @@ class City {
         }
     }
 
+    addForecast(forecast) {
+        this.forecast = forecast;
+    }
+
     displayCityWeather() {
-        document.getElementById("cityNameHeader").innerHTML = this.displayName;
+        const dataDiv = document.getElementById("dataDiv");
+
+        if (dataDiv.classList.contains('d-none')) {
+            dataDiv.classList.add('d-none');
+        }
+
+        document.getElementById("cityNameHeader").innerHTML = "Weather for: " + this.displayName;
         document.getElementById("country").innerHTML = this.data.sys.country;
         document.getElementById("weatherMain").innerHTML = this.data.weather[0].main;
         document.getElementById("weatherDesc").innerHTML = this.data.weather[0].description;
         const sunRiseDate = new Date(this.data.sys.sunrise * 1000);
-        document.getElementById("sunRise").innerHTML = sunRiseDate.getHours() + ":" + sunRiseDate.getMinutes() + ":" + sunRiseDate.getSeconds();
+        document.getElementById("sunRise").innerHTML = getStringDateFromUnix(sunRiseDate);
         const sunSetDate = new Date(this.data.sys.sunset * 1000);
-        document.getElementById("sunSet").innerHTML = sunSetDate.getHours() + ":" + sunSetDate.getMinutes() + ":" + sunSetDate.getSeconds();
-        document.getElementById("temp").innerHTML = this.data.main.temp;
-        document.getElementById("tempMax").innerHTML = this.data.main.temp_max;
-        document.getElementById("tempMin").innerHTML = this.data.main.temp_min;
-        document.getElementById("pressure").innerHTML = this.data.main.pressure;
-        document.getElementById("humidity").innerHTML = this.data.main.humidity;
-        document.getElementById("windSpeed").innerHTML = this.data.wind.speed;
+        document.getElementById("sunSet").innerHTML = getStringDateFromUnix(sunSetDate);
+        document.getElementById("temp").innerHTML = Math.ceil(this.data.main.temp) + "&#8451";
+        document.getElementById("tempMax").innerHTML = Math.ceil(this.data.main.temp_max) + "&#8451";
+        document.getElementById("tempMin").innerHTML = Math.ceil(this.data.main.temp_min) + "&#8451";
+        document.getElementById("pressure").innerHTML = this.data.main.pressure + "hPa";
+        document.getElementById("humidity").innerHTML = this.data.main.humidity + "%";
+        document.getElementById("windSpeed").innerHTML = this.data.wind.speed + "m/s";
         document.getElementById("winDeg").innerHTML = this.data.wind.deg;
         document.getElementById("weatherImage").src = this.icon;
+
+        this.displayChart();
+    }
+
+    displayChart() {
+        const ctx = document.getElementById('weatherChart');
+
+        //TODO:: Adjust displaying chart data
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+            },
+            options: {
+            scales: {
+                y: {
+                beginAtZero: true
+                }
+            }
+            }
+        });
     }
 
     pinCity() {
@@ -64,6 +100,20 @@ class City {
 getDataFromLocalStorage();
 displayPinnedCitiesAndLastBrowsedCities();
 
+if (pinnedCities.length > 0) {
+    const city = new City(null, null, null, pinnedCities[0]);
+    currCity = city;
+    city.displayCityWeather();
+} else if (lastBrowsedCities.length > 0) {
+    const city = new City(null, null, null, lastBrowsedCities[lastBrowsedCities.length - 1]);
+    currCity = city;
+    city.displayCityWeather();
+} else {
+    const dataDiv = document.getElementById("dataDiv");
+    dataDiv.classList.add('d-none');
+    document.getElementById("cityNameHeader").innerHTML = "Please search for city...";
+}
+
 function getDataFromLocalStorage() {
     const pinnedCitiesJson = localStorage.getItem("pinned");
     const lastCitiesJson = localStorage.getItem("history");
@@ -75,6 +125,30 @@ function displayPinnedCitiesAndLastBrowsedCities() {
     displayHistory();
     displayPinned();
     saveCurrentState();
+}
+
+function getStringDateFromUnix(date) {
+    let string = '';
+    
+    if (date.getHours() < 10) {
+        string += '0' + date.getHours() + ":";
+    } else {
+        string += date.getHours() + ":";
+    }
+
+    if (date.getMinutes() < 10) {
+        string += '0' + date.getMinutes() + ":";
+    } else {
+        string += date.getMinutes() + ":";
+    }
+
+    if (date.getSeconds() < 10) {
+        string += '0' + date.getSeconds();
+    } else {
+        string += date.getSeconds();
+    }
+    
+    return string;
 }
 
 function displayHistory() {
@@ -104,7 +178,7 @@ function displayPinned() {
 function createLiElement(city, mode) {
     const liElement = document.createElement('li');
     liElement.classList.add('left-side-li');
-    liElement.innerHTML = city.displayName + ", <span class='list-city-country'>" + city.data.sys.country + ", " + city.data.weather[0].main + ", " + city.data.main.temp + "&#8451; </span>";
+    liElement.innerHTML = city.displayName + ", <span class='list-city-country'>" + city.data.sys.country + ", " + city.data.weather[0].main + ", " + Math.ceil(parseInt(city.data.main.temp)) + "&#8451; </span>";
     const image = document.createElement('img');
     image.src = city.icon;
     liElement.appendChild(image);
@@ -156,6 +230,8 @@ async function searchWeatherForCity()
     const searchText = document.getElementById("city").value;
     const data = await fetchWeatherData(searchText);
     const city = new City(data, searchText, `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`, null);
+    const forecast = await fetchForecast(city.data.coord.lat, city.data.coord.lon);
+    city.addForecast(forecast);
     currCity = city;
     currCity.displayCityWeather();
     
@@ -168,14 +244,27 @@ async function searchWeatherForCity()
 
         displayPinnedCitiesAndLastBrowsedCities();
     } else {
-        alert("City not found");
+        const alertDialog = document.getElementById("limitDialog");
+        alertDialog.classList.remove('d-none');
+        setInterval(function () {
+            alertDialog.classList.add('d-none')
+        }, 5000);
     }
 }
 
 async function fetchWeatherData(searchText)
 {
-    response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${API_KEY}&units=metric`);
-    const data = await response.json()
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${API_KEY}&units=metric`);
+    const data = await response.json();
+
+    return data;
+}
+
+async function fetchForecast(lat, lon)
+{
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+    const data = await response.json();
+    console.log(data);
 
     return data;
 }
@@ -217,25 +306,26 @@ async function autoCompleteSuggest() {
         let cities = [];
         div.innerHTML = "";
     
-        xhr.open("GET", url);
-        xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            cities = JSON.parse(xhr.responseText).data;
-            cities.forEach(city => {
-                const newDiv = document.createElement("div");
-                newDiv.classList.add("auto-suggest-element");
-                newDiv.innerHTML = city.name + ", <span class='autos-suggest-country'>" + city.country + "</span>";
-                newDiv.addEventListener('click', function () {
-                    cityInputField.value = city.name;
-                    div.innerHTML = "";
-                });
-    
-                div.appendChild(newDiv);
-            });
-        }};
-        xhr.send();
+        const data = await fetch(url);
+        cities = await data.json();
+        await displaSugestions(cities);
     } else {
         div.innerHTML = "";
+    }
+}
+
+function displaSugestions(cities) {
+    const div = document.getElementById("autosuggest");
+    for (i = 0; i < cities.data.length; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("auto-suggest-element");
+        newDiv.innerHTML = cities.data[i].name + ", <span class='autos-suggest-country'>" + cities.data[i].country + "</span>";
+        newDiv.addEventListener('click', function () {
+            cityInputField.value = cities.data[i].name;
+            div.innerHTML = "";
+        });
+
+        div.appendChild(newDiv);
     }
 }
 
